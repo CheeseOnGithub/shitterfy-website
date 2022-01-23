d808d6a3fbfaf42a2382f1c58bdd3a99c5e41868
CheeseOnGithub <gamingman32@protonmail.com>
2022-01-23 20:15:34 +0100

    the sexer QAMONG
    Co-authored-by: NomNom <NomNomMonster@users.noreply.github.com>

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -4,6 +4,35 @@ import Header from './header';
 function About() {
   return <div>
       <Header />
+      <div className="aboutWrapper">
+        <h1 className='text'>What is <span className='SHITTER'> Shitify™©® </span> (big sex)</h1>
+        <p>Shitify is a multitool created by cheese#7654</p>
+      </div>
+
+      <div className="aboutWrapper">
+        <h1 className='text'>Why was <span className='SHITTER'> Shitify™©® </span> created?</h1>
+        <p>For absolutely no fucking reason, for shits and giggles even, it was mostly a passion project, made for fun, and to acquire knowledge about coding. </p>
+      </div>
+      
+      <div className="aboutWrapper">
+        <h1 className='text'>What is the point of <span className='SHITTER'> Shitify™©® </span></h1>
+        <p>A horrible multitool with spaghetti code, a dogshit array of tools, and an unmotivated lazy ass developer.  </p>
+        <p className='cunter'> <span >fuck off cunt - cheese </span>
+        </p>
+      </div>
+
+      <div className="aboutWrapper">
+        <h1 className='text'> <span className='the '>Credits</span></h1>
+        <p><span className='can_i_haz_chezburger_pls'>cheese#7654</span> - The lazy bastard creating this,  </p>
+        <p><span className='nomer'> NomNom#9561 </span> - "Helper" (Wrote the text in the about page, and made the app icon, other than that I did fuck all.), </p>
+        <p><span className='karolek'>karolek dulek#0932</span> - A retarded friend that was supposed to help, but instead forced us to play Apex Legends, 0/10 wouldn't play with again.</p>
+        <p><span className='racism'> K0wcio#0892 </span> - The most racist motherfucker I have ever seen/heard, he also looks like a minecraft skeleton.  <img src="https://cdn.discordapp.com/attachments/835103458565947403/934881739669639228/rainbow_six_siege_guide.png" alt="niga"/></p>
+        <p>Domino#7342 - Literal simp that fucked us over, left us to get some E-Bitches (Probably actually femboys), his IP address is: 178.214.5.6 </p> 
+        <p>cafe#0002 - Stupid dipshit that played Ultimate Custom Night in a voice call with us. And then proceeded to scream his lungs out</p> 
+      </div>
+
+      
+    <h6>shitify is not copyrighted, not registered, and not a trademark *wink wink*</h6>
   </div>;
 }
 
